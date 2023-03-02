4a6aae2dd16e6d326bfd065018dfbbe10f51a454
type0code <type0code@gmail.com>
2023-03-02 17:17:42 +0200

    cleared repo

--- src/app/interfaces/app-state.ts
+++
@@ -1,7 +0,0 @@
-import { IPageData } from './page-data';
-import { IAppSettings } from './settings';
-
-export interface IAppState {
-  pageData: IPageData;
-  appSettings: IAppSettings
-}
--- src/app/ui/components/color-picker/color-picker.component.spec.ts
+++
@@ -1,4 +0,0 @@
-import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
-
-import { TCColorPickerComponent } from './color-picker.component';
-
